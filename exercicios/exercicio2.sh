c0d9d423c40148e9028fa3d0f1e7836bb73209f8
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-07-14 15:57:45 -0300

    Create exercicio2.sh

---
+++ exercicios/exercicio2.sh
@@ -0,0 +1,23 @@
+###########################################################################
+#                                                                                    
+# exercicio2.sh                                                                     
+#                                                                                    
+# Autor: Matheus Silvano (matheussilvano2005@gmail.com)                              
+# Data Criação: 14/07/2024                                                           
+#                                                                                    
+# Descrição: Crie um script que gere um relatório de algumas informações da máquina atual:
+# • Nome da Máquina
+# • Data e Hora Atual
+# • Desde quando a máquina está ativa
+# • Versão do Kernel
+# • Quantidade de CPUs/Cores
+# • Modelo da CPU
+# • Total de Memória RAM Disponível
+# • Partições              
+#                                                                                    
+# Exemplo de uso: ./exercicio2.sh                                                     
+#                                                                                    
+######################################################################################
+
+NomeMaquina=uname
+DataHoraAtual=date
