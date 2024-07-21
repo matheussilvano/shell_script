a70fb2726ece0daca20b95c263bee06f205ab4c4
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-07-21 17:26:59 -0300

    Update SaudacaoPorHorario.sh

--- exercicios/InstrucoesCondicionais/SaudacaoPorHorario.sh
+++ exercicios/InstrucoesCondicionais/SaudacaoPorHorario.sh
@@ -5,15 +5,15 @@ HoraAtual=$(date +"%H")
 # Hora e minuto atual
 HoraMinutoAtual=$(date +"%H:%M")
 
-if ["$HoraAtual" -ge 6 -a -lt 12]
-  then
-    echo "Bom dia!"
-elif ["$HoraAtual" -ge 12 -a -lt 18]
-  then
-    echo "Boa tarde!"
+if [ "$HoraAtual" -ge 6 -a "$HoraAtual" -lt 12 ]
+then
+  echo "Bom dia!"
+elif [ "$HoraAtual" -ge 12 -a "$HoraAtual" -lt 18 ]
+then
+  echo "Boa tarde!"
 else
-    echo "Boa noite!"
+  echo "Boa noite!"
 fi
 
     
-  
+echo "A hora atual é: $HoraMinutoAtual"
